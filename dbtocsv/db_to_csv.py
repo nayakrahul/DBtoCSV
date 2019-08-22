8017aed02d95c50277d770597f61f8d48e08b290
nayakrahul <rahulnk521@gmail.com>
2019-08-22 16:11:21 +0530

    include exclude options added

--- dbtocsv/db_to_csv.py
+++ dbtocsv/db_to_csv.py
@@ -1,5 +1,9 @@
+from halo import Halo
+
 from .db_reader import Database
 from .csv_writer import CSV
+
+spinner = Halo(spinner='arrow3')
 
 
 class DBtoCSV:
@@ -26,20 +30,37 @@ class DBtoCSV:
         except:
             raise Exception("Error while making database connection.")
 
-    def write_to_csv(self):
+    def write_to_csv(self, only_tables=[], exclude_tables=[]):
+        """
+        Parameters
+        ----------
+        only_tables : list(str), optional
+            fetch for only these tables.
+        exclude_tables : list(str), optional
+            exclude these tables.
+        """
         try:
-            tables = self.db.fetch_tables()
+            if len(only_tables) > 0:
+                tables = only_tables
+            else:
+                tables = self.db.fetch_tables()
         except:
             raise Exception("Error while fetching tables.")
+        tables = list(set(tables) - set(exclude_tables))
 
         for table in tables:
+            spinner.text = "Writing table {}".format(table)
+            spinner.start()
             try:
                 columns = self.db.fetch_columns(table)
                 rows = self.db.fetch_all_rows(table)
             except:
                 raise Exception("Error while fetching data from tables.")
+                spinner.fail("Writing to table {} failed".format(table))
             try:
                 csv = CSV(file='{}.csv'.format(table))
                 csv.write(rows, columns)
+                spinner.succeed("Wrote table {}".format(table))
             except:
                 raise Exception("Error while writing data to csv file.")
+                spinner.fail("Writing to table {} failed".format(table))
