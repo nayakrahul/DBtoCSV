b3624f483d513f42c555bd18e7225493c7782d6e
nayakrahul <rahulnk521@gmail.com>
2019-08-22 14:39:48 +0530

    long description added

--- setup.py
+++ setup.py
@@ -1,10 +1,14 @@
 from setuptools import setup, find_packages
 from os import path
+import io
 
 current_path = path.abspath(path.dirname(__file__))
 
 with open(path.join(current_path, 'requirements.txt'), encoding='utf-8') as f:
     all_reqs = f.read().split('\n')
+
+with io.open("README.md", encoding='utf-8') as infile:
+    long_description = infile.read()
 
 install_requires = [x.strip() for x in all_reqs if 'git+' not in x]
 dependency_links = [x.strip().replace('git+', '')
@@ -13,8 +17,10 @@ dependency_links = [x.strip().replace('git+', '')
 
 setup(
      name='dbtocsv',
-     version='0.0.1',
+     version='0.1.0',
      description="Python Script to put data from MySQL database into csv files",
+     long_description=long_description,
+     long_description_content_type="text/markdown",
      url="https://github.com/nayakrahul/DBtoCSV",
      keywords='mysql database csv',
      packages=find_packages(),
